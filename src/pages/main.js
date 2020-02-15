239c6f951a7be8424dde233ce25bb7c8c28bcac1
KamilLatipov <latipovkamil@mail.ru>
2020-02-15 17:37:35 +0300

    footer-page style

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,3 +3,5 @@ import '../blocks/header/header.js';
 import '../blocks/main-nav/main-nav.js';
 import './global.scss';
 import '../fonts/fonts.scss';
+import '../blocks/footer/footer.js';
+import '../blocks/footer-nav/footer-nav.js';
