fc06ca7700f2f82ee0b4dae619579b249d8b07fd
KamilLatipov <latipovkamil@mail.ru>
2020-05-16 13:34:30 +0300

    Добавил стили для h3

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import '../blocks/header/header.js';
 import '../blocks/main-nav/main-nav.js';
-import './global.scss';
+import '../style/global.scss';
 import '../fonts/fonts.scss';
 import '../blocks/footer/footer.js';
 import '../blocks/footer-nav/footer-nav.js';
