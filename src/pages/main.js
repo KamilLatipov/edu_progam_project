9a7727a393ba1a91b459025e454ad6b10cddf728
KamilLatipov <latipovkamil@mail.ru>
2020-06-05 15:53:13 +0300

    Добавил room-number mixin

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,6 +2,9 @@ import _ from 'lodash';
 import '../blocks/header/header.js';
 import '../blocks/main-nav/main-nav.js';
 import '../style/global.scss';
-import '../fonts/fonts.scss';
+import '../style/fonts.scss';
 import '../blocks/footer/footer.js';
 import '../blocks/footer-nav/footer-nav.js';
+
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
