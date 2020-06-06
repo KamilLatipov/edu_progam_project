d4d33002d57ecf49d6d4c219f863446a5717216d
KamilLatipov <latipovkamil@mail.ru>
2020-06-06 14:34:10 +0300

    Добавил text-field mixin

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   plugins: [	
      new HtmlWebpackPlugin({
        filename: 'index.html',
-       template: './src/pages/room-details/room-details.pug'
+       template: './src/pages/registration/registration.pug'
      }),
  	  ],
   output: {
