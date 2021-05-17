c421dc7e85e0b3d6bd4bb6640e1b340e5840084a
Kamil <latipovkamil@mail.ru>
2021-05-17 13:03:40 +0300

    (dropdown)Перенес создание параметров из js в Pug

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,30 +1,9 @@
-const dropdownElem = document.getElementsByClassName('dropdown__hidden-part');
-let i;
-for (i = 0; i < dropdownElem.length; i++) {
-    initDropdown(dropdownElem[i]);
-}
+//const dropdownElem = document.getElementsByClassName('dropdown__parameters');
+//let i;
+//for (i = 0; i < dropdownElem.length; i++) {
+//    initDropdown(dropdownElem[i]);
+//}
 
 function initDropdown(dropdownItem) {
     const inputFieldValues = dropdownItem.getAttribute('data-input-field').split(';');
-    const paramArray = dropdownItem.getAttribute('data-parameters').split(';');
-    const clearBtn = dropdownItem.getAttribute('data-clear-btn');
-
-    addDropdownParameters(paramArray, dropdownItem);
-}
-
-function addDropdownParameters(paramArray, dropdownItem) {
-    let parameter;
-
-    for (i = 0; i < paramArray.length; i++) {
-        parameter = createDropdownParamElem(paramArray[i]);
-        dropdownItem.append(parameter);
-    }
-}
-
-function createDropdownParamElem(paramArrayItem) {
-    let parameter
-    parameter = document.createElement('li');
-    parameter.innerText = paramArrayItem;
-
-    return (parameter);
 }
